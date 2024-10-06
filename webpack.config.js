6efd5fe650b37d7c4e2ba93f83629bd136279717
Iktedar <ansariiktedar246@gmail.com>
2024-10-07 01:18:17 +0530

    Initial Webpack config commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true, // Clean the output directory before each build
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/, // For CSS files
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 3000, // Development server port
+    hot: true, // Enable hot module replacement
+    open: true, // Open browser automatically
+  },
+  mode: 'development', // For development; change to 'production' for production builds
+//   extensions: ['.js', '.jsx'],
+};
